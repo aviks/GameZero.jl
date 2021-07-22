1fa61d8ec299bb2d0f09f12fd016718168b2867f
Mosè Giordano <mose@gnu.org>
2021-07-22 15:19:39 +0100

    Pass mandatory argument to `initgame` in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,16 +3,16 @@ using Test
 
 @testset "basic" begin
     global g
-    @test_nowarn begin 
-        g = GameZero.initgame(joinpath("..","example","BasicGame","basic.jl"))
+    @test_nowarn begin
+        g = GameZero.initgame(joinpath("..","example","BasicGame","basic.jl"), true)
         GameZero.quitSDL(g)
     end
-    
+
 end
 
 @testset "basic2" begin
-    @test_nowarn begin 
-        g = GameZero.initgame(joinpath("..","example","BasicGame","basic2.jl"))
+    @test_nowarn begin
+        g = GameZero.initgame(joinpath("..","example","BasicGame","basic2.jl"), true)
         GameZero.quitSDL(g)
     end
 end
